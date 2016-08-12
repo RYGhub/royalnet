43d5bec4b78eafbb262151465237105a8ac687ac
Stefano Pigozzi <ste.pigozzi@gmail.com>
2016-08-12 17:23:45 +0200

    Moved some things around

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import asyncio
 import discord
-client = discord.Client()
+import json
+import overwatch
+d_client = discord.Client()
+
+# Get player database from the db.json file
+file = open("db.json")
+db = json.load(file)
+file.close()
 
 
 # When the discord client is ready, print something
-@client.event
+@d_client.event
 async def on_ready():
     print("Connessione riuscita!")
-    print(client.user.name)
-    print(client.user.id)
+    print(d_client.user.name)
+    print(d_client.user.id)
+
 
 # Get the discord bot token from "discordtoken.txt"
 f = open("discordtoken.txt", "r")
@@ -16,4 +24,11 @@ token = f.read()
 f.close()
 
 # Start discord bot client but ignore events
-client.start(token)
+d_client.start(token)
+
+# List overwatch players
+ow_players = list()
+for player in db:
+    if db[player]["overwatch"] is not None:
+        ow_players.append(db[player]["overwatch"])
+
--- overwatch.py
+++ overwatch.py
@@ -1,18 +1,6 @@
 import asyncio
 import requests
-import json
 loop = asyncio.get_event_loop()
-
-# Get player database from the db.json file
-file = open("db.json")
-db = json.load(file)
-file.close()
-
-# List overwatch players
-players = list()
-for player in db:
-    if db[player]["overwatch"] is not None:
-        players.append(db[player]["overwatch"])
 
 # Get player data
 async def get_player_data(platform: str, region: str, battletag: str):
@@ -26,5 +14,3 @@ async def get_player_data(platform: str, region: str, battletag: str):
         return r.json()
     elif r.status_code == 404:
         raise Exception("Player not found.")
-
-loop.run_until_complete(get_player_data("pc", "eu", "SteffoRYG#2876"))
