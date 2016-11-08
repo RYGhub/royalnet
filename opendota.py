ac62ac1897b0800abd9bd7c08843b75da11ae798
Stefano Pigozzi <ste.pigozzi@gmail.com>
2016-11-08 21:07:23 +0100

    Now it works

--- opendota.py
+++ opendota.py
@@ -2,7 +2,8 @@ import asyncio
 import requests
 loop = asyncio.get_event_loop()
 
-async def get_latest_match(steamidtre: int):
+async def get_latest_match(steamidtre: str):
+    steamidtre = steamidtre[1:-1].split(":")[2]
     print("[OpenDota] Getting latest match for: {steamid}".format(steamid=steamidtre))
     r = await loop.run_in_executor(None, requests.get, 'https://api.opendota.com/api/players/{steamidtre}/matches?limit=1'.format(steamidtre=steamidtre))
     if r.status_code == 200:
