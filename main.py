1a44f1d869f8b6814ab63f63e43a17dad5a3e9e2
Stefano Pigozzi <ste.pigozzi@gmail.com>
2017-02-07 22:26:53 +0100

    Osu!

--- main.py
+++ main.py
@@ -327,7 +327,7 @@ async def osu_pp(timeout):
             print("[Osu!] Check successful.")
             await asyncio.sleep(timeout)
         else:
-            await asyncio.sleep(1)
+            await asyncio.sleep(5)
 
 
 
@@ -353,14 +353,14 @@ async def send_event(eventmsg: str, player: str, **kwargs):
     # Send the message
     loop.create_task(telegram.send_message(msg, -2141322))
 
-#loop.create_task(overwatch_status_change(600))
-#print("[Overwatch] Added level up check to the queue.")
+loop.create_task(overwatch_status_change(900))
+print("[Overwatch] Added level up check to the queue.")
 
-#loop.create_task(league_rank_change(900))
-#print("[League] Added rank change check to the queue.")
+loop.create_task(league_rank_change(900))
+print("[League] Added rank change check to the queue.")
 
-#loop.create_task(league_level_up(900))
-#print("[League] Added level change check to the queue.")
+loop.create_task(league_level_up(900))
+print("[League] Added level change check to the queue.")
 
 #loop.create_task(brawlhalla_update_mmr(7200))
 #print("[Brawlhalla] Added mmr change check to the queue.")
@@ -368,7 +368,7 @@ async def send_event(eventmsg: str, player: str, **kwargs):
 #loop.create_task(opendota_last_match(600))
 #print("[OpenDota] Added last match check to the queue.")
 
-loop.create_task(osu_pp(600))
+loop.create_task(osu_pp(1800))
 print("[Osu!] Added pp change check to the queue.")
 
 try:
