1e9c3ea0f817b4c5a2ece013eef104130187f6d7
Stefano Pigozzi <ste.pigozzi@gmail.com>
2017-04-20 22:37:47 +0200

    lol ma era così semplice

--- main.py
+++ main.py
@@ -68,22 +68,23 @@ async def overwatch_status_change(timeout):
                             # Update database
                             db[player]["overwatch"]["level"] = level
                         # Check for rank changes
-                        # rank = r["data"]["competitive"]["rank"]
-                        # if rank is not None:
-                        #     rank = int(rank)
-                        #     try:
-                        #         oldrank = int(db[player]["overwatch"]["rank"])
-                        #     except KeyError:
-                        #         oldrank = 0
-                        #     if rank != oldrank:
-                        #         # Send the message
-                        #         loop.create_task(send_event(eventmsg=s.overwatch_rank_change,
-                        #                                     player=player, change=overwatch.format_rankchange(rank-oldrank),
-                        #                                     rank=rank, medal=overwatch.url_to_medal(r["data"]["competitive"]["rank_img"])))
-                        #         # Update database
-                        #         db[player]["overwatch"]["rank"] = rank
-                        # else:
-                        db[player]["overwatch"]["rank"] = None
+                        rank = r["data"]["competitive"]["rank"]
+                        if rank is not None:
+                            rank = int(rank)
+                            try:
+                                oldrank = int(db[player]["overwatch"]["rank"])
+                            except KeyError:
+                                oldrank = 0
+                            if rank != oldrank:
+                                # Send the message
+                                loop.create_task(send_event(eventmsg=s.overwatch_rank_change,
+                                                            player=player, change=overwatch.format_rankchange(rank-oldrank),
+                                                            rank=rank, medal=overwatch.url_to_medal(r["data"]["competitive"]["rank_img"])))
+                                # Update database
+                                db[player]["overwatch"]["season"] = 4
+                                db[player]["overwatch"]["rank"] = rank
+                        else:
+                            db[player]["overwatch"]["rank"] = None
                         save_db()
                     finally:
                         await asyncio.sleep(1)
