8c5776b9d295ac60c9e4f3a7cfe817ecb88fd9dc
Stefano Pigozzi <me@steffo.eu>
2024-07-14 04:42:04 +0200

    Add `/cat` command

---
+++ src/services/telegram/commands/cat.rs
@@ -0,0 +1,42 @@
+use anyhow::Context;
+use reqwest::Url;
+use teloxide::Bot;
+use teloxide::payloads::SendPhotoSetters;
+use teloxide::requests::Requester;
+use teloxide::types::{InputFile, Message};
+use serde::Deserialize;
+use super::{CommandResult};
+
+
+const CAT_API_URL: &str = "https://api.thecatapi.com/v1/images/search";
+
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
+struct CatQueryResponse {
+	// id: String,
+	pub url: String,
+	// width: usize,
+	// height: usize,
+}
+
+
+pub async fn handler(bot: &Bot, message: &Message) -> CommandResult {
+	let response = reqwest::get(CAT_API_URL).await
+		.context("Non è stato possibile richiedere un gatto all'API.")?
+		.json::<Vec<CatQueryResponse>>().await
+		.context("Il gatto ricevuto in risposta dall'API è indecifrabile, quindi non è stato possibile riceverlo.")?
+		.pop()
+		.context("Il gatto ricevuto in risposta dall'API non esiste, quindi non è stato possibile riceverlo.")?;
+
+	let url: Url = response.url.parse()
+		.context("L'URL del gatto ricevuto in risposta dall'API è malformato, quindi non è stato possibile riceverlo.")?;
+
+	let input = InputFile::url(url);
+
+	let _reply = bot
+		.send_photo(message.chat.id, input)
+		.reply_to_message_id(message.id)
+		.await
+		.context("Non è stato possibile inviare un gatto in risposta a questo messaggio.")?;
+
+	Ok(())
+}
