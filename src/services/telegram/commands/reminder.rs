9f920ffcec0aa98350d8703bc61337d6ab003e66
Stefano Pigozzi <me@steffo.eu>
2024-08-14 12:05:50 +0200

    Extract `determine_wait` to `crate::utils`

--- src/services/telegram/commands/reminder.rs
+++ src/services/telegram/commands/reminder.rs
@@ -8,17 +8,9 @@ use parse_datetime::parse_datetime_at_date;
 use once_cell::sync::Lazy;
 use regex::Regex;
 use crate::utils::telegramdisplay::TelegramEscape;
+use crate::utils::time::determine_wait;
 use super::{CommandResult};
 
-
-fn determine_wait(target_chrono: chrono::DateTime<chrono::Local>) -> tokio::time::Duration {
-	let now_chrono = chrono::Local::now();
-
-	let duration_chrono = target_chrono.signed_duration_since(now_chrono);
-	let seconds = duration_chrono.num_seconds();
-
-	tokio::time::Duration::from_secs(seconds as u64)
-}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ReminderArgs {
---
+++ src/utils/time.rs
@@ -0,0 +1,8 @@
+pub fn determine_wait(target_chrono: chrono::DateTime<chrono::Local>) -> tokio::time::Duration {
+	let now_chrono = chrono::Local::now();
+
+	let duration_chrono = target_chrono.signed_duration_since(now_chrono);
+	let seconds = duration_chrono.num_seconds();
+
+	tokio::time::Duration::from_secs(seconds as u64)
+}
