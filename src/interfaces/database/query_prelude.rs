4bd14da04cafb977533bbef0e2637d19b3ad8f60
Stefano Pigozzi <me@steffo.eu>
2024-08-19 08:23:09 +0200

    RustRover please stop

--- src/interfaces/database/query_prelude.rs
+++ src/interfaces/database/query_prelude.rs
@@ -1,9 +1,12 @@
-#![allow(unused_imports)]
-
 pub use diesel::dsl::*;
 pub use diesel::ExpressionMethods;
 pub use diesel::OptionalExtension;
 pub use diesel::query_dsl::*;
 pub use diesel::SelectableHelper;
 pub use diesel::upsert::*;
+
+#[allow(unused_imports)]
+pub use super::models::*;
+#[allow(unused_imports)]
+pub use super::schema::*;
 
